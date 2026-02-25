a869bead97222b13a563f16a69654dddf4cb6c03
pbutterworth <3683210+pbutterworth@users.noreply.github.com>
2026-02-26 11:30:41 +1300

    Fixed bug with parallel task async_forward_entry_setups

--- custom_components/astralpool_chlorinator/__init__.py
+++ custom_components/astralpool_chlorinator/__init__.py
@@ -44,9 +44,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         entry.title, chlorinator, coordinator
     )
 
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
